13273e15be145ec09e07074deb2b9d1469693f58
prank-vish <prankvish@gmail.com>
2024-11-03 15:32:19 +0530

    oiuh

--- p.cpp
+++ p.cpp
@@ -8,6 +8,7 @@ protected:
     bool isAvailable = true;
 
 public:
+    Book(){};
     Book(string t, string a, string p, string id, bool status) : title(t), author(a), publication(p), isbn(id), isAvailable(status) {}
     Book get_details(const string &line)
     {
@@ -78,11 +79,24 @@ public:
 
 class Student
 {
+protected:
     string studentId, name, email;
     vector<Book> borrowedBooks;
 
 public:
+Student(){};
     Student(string ID, string n, string e) : studentId(ID), name(n), email(e) {}
+    Student get_student(const string &line)
+    {
+        istringstream iss(line);
+        string name, email, studentId;
+
+        getline(iss, studentId, '\t');
+        getline(iss, name, '\t');
+        getline(iss, email, '\t');
+
+        return Student(studentId, name, email);
+    }
     void borrowBook(Book &book)
     {
         
@@ -115,6 +129,86 @@ public:
     {
         cout<<"Member Details : ";
         cout<<"Student ID : "<<studentId<<", Name : "<<name<<", Email : "<<email<<endl;
+    }
+};
+class Library : public Book, public Student
+{
+public:
+    Library(){};
+    void addbook(string tit, string auth, string publish, string id, bool isava)
+    {
+        // Book b1(tit,auth,publish,id,isava);
+        ofstream writef("books.txt", ios::app);
+        if (writef.is_open())
+        {
+            writef << isbn << "\t" << title << "\t" << author << "\t" << publication << "\t" << isAvailable << "\n";
+            writef.close();
+        }
+        else
+        {
+            cerr << "Unable to open the file " << endl;
+        }
+    }
+    void addstudent(string studentId, string name, string email)
+    {
+        ofstream writef("students.txt", ios::app);
+        if (writef.is_open())
+        {
+            writef << studentId << "\t" << name << "\t" << email << "\n";
+            writef.close();
+        }
+        else
+        {
+            cerr << "Unable to open the file " << endl;
+        }
+    }
+    // void removebook(){
+
+    // }
+    // void removestudent(){
+
+    // }
+    void searchbook(string id)
+    {
+        Available(id);
+    }
+    void dis_book()
+    {
+        ifstream readf("books.txt");
+        if (readf.is_open())
+        {
+
+            string line;
+            while (getline(readf, line))
+            {
+                Book book = get_details(line);
+                book.display();
+            }
+            readf.close();
+        }
+        else
+        {
+            cout << "unable to open" << endl;
+        }
+    }
+    void dis_student()
+    {
+        ifstream readf("students.txt");
+        if (readf.is_open())
+        {
+
+            string line;
+            while (getline(readf, line))
+            {
+                Student student = get_student(line);
+                student.studentDetails();
+            }
+            readf.close();
+        }
+        else
+        {
+            cout << "unable to open" << endl;
+        }
     }
 };
 int main()
